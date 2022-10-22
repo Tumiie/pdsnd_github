f0d77252d29e827688f37a4d12fa0bb205f38dd8
Tumelo Matobo <tumelotumiematobo@gmail.com>
2022-10-22 22:52:08 +0200

    refactor:Removed the get filters function from code

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -6,27 +6,6 @@ CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
 
-def get_filters():
-    """
-    Asks user to specify a city, month, and day to analyze.
-
-    Returns:
-        (str) city - name of the city to analyze
-        (str) month - name of the month to filter by, or "all" to apply no month filter
-        (str) day - name of the day of week to filter by, or "all" to apply no day filter
-    """
-   
-    # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
-
-
-    # get user input for month (all, january, february, ... , june)
-
-
-    # get user input for day of week (all, monday, tuesday, ... sunday)
-
-
-    
-    return city, month, day
 
 
 def load_data(city, month, day):
